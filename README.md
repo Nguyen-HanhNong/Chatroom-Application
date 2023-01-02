8fbdeeb6067d839ebbd2b6626652a658f9f19c93
Nguyen-HanhNong <nongnguyenhanh@gmail.com>
2023-01-02 16:07:13 -0500

    created css folder and updated web pages

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Repository containing a basic chat application created using Django and Python
 
 
 python manage.py runserver
+./manage.py migrate
+./manage.py reset_db
--- djangochat/settings.py
+++ djangochat/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chat'
+    'chatroom',
 ]
 
 MIDDLEWARE = [
@@ -118,7 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
